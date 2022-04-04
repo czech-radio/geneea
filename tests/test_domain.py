9e4408615760142b8bc7442ff67b37957b2edf1d
David Landa <david.landa@protonmail.com>
2022-04-04 10:46:06 +0200

    Fix test

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -5,4 +5,4 @@ import pytest
 @pytest.mark.domain
 def test_model():
     from cro.geneea._datamodel import Datamodel
-    assert Datamodel(None) is not None
+    assert Datamodel('{"text": "Toto je testovací věta"}') is not None
