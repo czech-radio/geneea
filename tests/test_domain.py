7b7fe16debc24984faf4138f5ab70ef16d307e83
David Landa <david.landa@protonmail.com>
2022-04-04 10:47:43 +0200

    Use black

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,8 +1,8 @@
-
 import pytest
 
 
 @pytest.mark.domain
 def test_model():
     from cro.geneea._datamodel import Datamodel
+
     assert Datamodel('{"text": "Toto je testovací věta"}') is not None
