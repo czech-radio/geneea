ba862e06902b39a8406799499b207a9fbcb892fb
David Landa <david.landa@protonmail.com>
2022-04-04 14:20:58 +0200

    Add new client methods

--- src/cro/geneea/__main__.py
+++ src/cro/geneea/__main__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
+"""
+The command line interface.
+"""
+
+
 import argparse
 import os
 
 from cro.geneea._client import Client as GeneeaClient
 
-"""
-The command line interface.
-"""
 
-if __name__ == "__main__":
+def main():
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("-f", "--file", required=True, type=str, help="source filename")
+    parser.add_argument("-f", "--file", required=True, type=str, help="The file name")
+    parser.add_argument("-t", "--type", required=True, type=str, help="The operation type")
 
     args = parser.parse_args()
 
@@ -21,6 +23,37 @@ if __name__ == "__main__":
 
     client = GeneeaClient(key=KEY)
 
-    result = client.analyze("Toto je testovací věta!")
+    phrases = GeneeaClient.read_phrases(args.file)
 
-    print(result)
+    if (args.type == "analysis"):
+        # ANALYSIS
+        print("ANALYSIS\n--------")
+        result = client.get_analysis(phrases[0])
+        print(result)
+    elif (args.type == "account"):
+        # ACCOUNT
+        print("ACCOUNT\n--------")
+        result = client.get_account()
+        print(result)
+    elif (args.type == "entities"):
+        # ENTITIES
+        print("ENTITIES\n--------")
+        result = client.get_entities(phrases[0])
+        print(result)
+    elif (args.type == "tags"):
+        # TAGS
+        print("TAGS\n--------")
+        result = client.get_tags(phrases[0])
+        print(result)
+    elif (args.type == "sentiment"):
+        # SENTIMENT
+        print("SENTIMENT\n--------")
+        result = client.get_sentiment(phrases[0])
+        print(result)
+    elif (args.type == "relations"):
+        # REALATION
+        print("RELATIONS\n--------")
+        result = client.get_relations(phrases[0])
+        print(result)
+    else:
+        print("Choose one of the following type: 'analysis', 'account', 'entities', 'tags', 'sentiment', 'relations'")
--- src/cro/geneea/_client.py
+++ src/cro/geneea/_client.py
@@ -1,63 +1,149 @@
 # -*- coding: utf-8 -*-
 
-import logging
-import sys
 
-from requests import post
+import logging
+
+from requests import post, get
 
 from cro.geneea._datamodel import Datamodel
 
 
 class Client:
     """
-    Geneea API Client
+    Geneea REST API client for https://api.geneea.com/
+
+    Errors e.g.:
+    {'exception': 'Exception', 'message': 'The requested resource is not available.'}
     """
 
-    __URL__ = "https://api.geneea.com/v3"
+    __URL__ = "https://api.geneea.com/"
 
     def __init__(self, key: str):
         self._key = key
+        self.headers = {
+            "content-type": "application/json",
+            "Authorization": f"user_key {self.key}",
+        }
 
     @property
     def key(self) -> str:
         return self._key
 
     @classmethod
-    def get_txt(cls, file_name):
-        raw = ""
-        print("Processing file: ", file_name)
-        with open(file_name, encoding="utf8") as file:
-            line = file.readline()
-            while line:
-                line = file.readline()
-                raw = raw + line
-        return raw
+    def read_phrases(cls, path) -> list[str]:
+        with open(path, encoding="utf8") as file:
+            lines = file.readlines()
+            return lines
 
-    def analyze(self, input) -> str:
+    def get_account(self) -> dict:
         """
-        Analyze a given input text.
+        Get account information.
+        """
+        try:
+            response = get(f"{self.__URL__}/account", headers=self.headers)
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
+        except Exception as ex:
+            logging.error(ex)
+            raise ex
+
+    def get_analysis(self, input) -> dict:
+        """
+        get analysis for the given input text.
 
         :param input: The input text to analyze.
         :return The analyzed input text.
+
+        See example JSON output in `data` folder.
         """
         try:
-            headers = {
-                "content-type": "application/json",
-                "Authorization": f"user_key {self.key}",
-            }
-            data = post(
-                f"{self.__URL__}/analysis", json={"text": input}, headers=headers
+            response = post(
+                f"{self.__URL__}/v3/analysis", json={"text": input}, headers=self.headers
             )
-
-            return data.json()
-
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
         except Exception as ex:
-            logging.error(f"Connection error {ex}")
+            logging.error(ex)
             raise ex
 
-    @classmethod
-    def deserialize(cls, input: dict) -> Datamodel:
+    def get_entities(self, input) -> dict:
         """
-        TODO Convert JSON to model.
+        Get entites for the given input text.
+
+        :param input: The input text to analyze.
+        :return The analyzed input text.
+
+        See example JSON output in `data` folder.
         """
-        return Datamodel(input)
+        try:
+            response = post(
+                f"{self.__URL__}/v3/entities", json={"text": input}, headers=self.headers
+            )
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
+        except Exception as ex:
+            logging.error(ex)
+            raise ex
+
+    def get_tags(self, input) -> dict:
+        """
+        Get tags for the given input text.
+
+        :param input: The input text to analyze.
+        :return The analyzed input text.
+
+        See example JSON output in `data` folder.
+        """
+        try:
+            response = post(
+                f"{self.__URL__}/v3/tags", json={"text": input}, headers=self.headers
+            )
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
+        except Exception as ex:
+            logging.error(ex)
+            raise ex
+
+    def get_sentiment(self, input) -> dict:
+        """
+        Get sentiment for the given input text.
+
+        :param input: The input text to analyze.
+        :return The analyzed input text.
+
+        See example JSON output in `data` folder.
+        """
+        try:
+            response = post(
+                f"{self.__URL__}/v3/sentiment", json={"text": input}, headers=self.headers
+            )
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
+        except Exception as ex:
+            logging.error(ex)
+            raise ex
+
+    def get_relations(self, input) -> dict:
+        """
+        Get relation for the given input text.
+
+        :param input: The input text to analyze.
+        :return The analyzed input text.
+
+        See example JSON output in `data` folder.
+        """
+        try:
+            response = post(
+                f"{self.__URL__}/v3/relations", json={"text": input}, headers=self.headers
+            )
+            logging.info(response.status_code)
+            # @todo Check status code.
+            return response.json()
+        except Exception as ex:
+            logging.error(ex)
+            raise ex
