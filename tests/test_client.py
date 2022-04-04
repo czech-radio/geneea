28ba850c97adcf0fd3a8ef15867a48f17bdd105f
David Landa <david.landa@protonmail.com>
2022-04-04 11:07:01 +0200

    Add test marks

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,17 @@
 import pytest
 
-from cro.geneea import __version__
+from cro.geneea import __version__, Client
 
 """
 Package tests
 -------------
 Test the project as a package e.g. check the version, style etc.
 """
+
+
+@pytest.mark.client
+def test_client():
+    assert Client(key=None) is not None
 
 
 def test_version():
