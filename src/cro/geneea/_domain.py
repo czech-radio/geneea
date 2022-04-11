c1748a42a0075dc59a7e8307d006ca6d6c657a3f
Krystof Pesek <krystof.pesek@gmail.com>
2022-04-11 13:46:50 +0200

    model in domain file

--- src/cro/geneea/_domain.py
+++ src/cro/geneea/_domain.py
@@ -2,7 +2,7 @@
 
 
 from __future__ import annotations
-
+from collections import namedtuple as NamedTuple
 from typing import Optional
 
 import json
@@ -53,3 +53,58 @@ class Text:
     def sentiment(self) -> object:
         _sentiment = self.analyzed["docSentiment"]
         return _sentiment
+
+
+class Analysis:
+    """
+    top level class Analysis
+    encapsluates lower level classes
+    """
+
+    text: Text
+    language: str
+    entities: List[Entity]
+    tags: List[Tag]
+    sentiment: Sentiment
+    relations: List[Relation]
+
+
+class Sentiment(NamedTuple):
+    """
+    originally docSentiment, it keeps sentiment of a whole document
+    """
+
+    mean: float
+    label: str
+    positive: float
+    negative: float
+
+
+class Entity(NamedTuple):
+    """
+    exctracted entities, known ones gets gkbId
+    """
+
+    id: str
+    gkbId: str
+    text: str
+    type: str
+
+
+class Relation(NamedTuple):
+    """
+    TODO: entitiy connection?
+    """
+
+    name: str
+    type: str
+    entity: Optional[Entity]
+
+
+class Tag(NamedTuple):
+    """
+    tags derived from text
+    """
+
+    id: str
+    text: str
