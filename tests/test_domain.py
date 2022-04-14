eb06e13737070463db68d5d62a83c8a50e923417
Krystof Pesek <krystof.pesek@gmail.com>
2022-04-14 11:15:17 +0200

    test instancing objects

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -3,11 +3,31 @@
 
 import pytest
 
-from cro.geneea._domain import Model
-
+from cro.geneea._domain import Model,Entity,Sentiment,Relation,Tag
 
 @pytest.mark.domain
 def test_text_model():
     assert (
         Model(original="Toto je testovací věta", analyzed={"test": "test"}) is not None
+    )
+
+
+def test_entity():
+    assert (
+        Entity("id_data","stdForm_data","type_data") is not None
+    )
+
+def test_sentiment():
+    assert (
+        Sentiment("mean_data","label_data","1.0","-0.0") is not None
+    )
+
+def test_relation():
+    assert (
+        Relation("id_data","name_data","textRepr_data","type_data",["args_data","args_data"]) is not None
+    )
+
+def test_tag():
+    assert (
+        Tag("id_data","stdForm_data","type_data","relevance_data") is not None
     )
