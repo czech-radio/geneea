1e6407c59ccc92842e9b37933ef39f9c598a7bb8
David Landa <david.landa@protonmail.com>
2022-04-20 20:59:03 +0200

    Add sdk subpackage

--- src/cro/geneea/__init__.py
+++ src/cro/geneea/__init__.py
@@ -1,9 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from cro.geneea.sdk._client import Client
-from cro.geneea.sdk._domain import Analysis, Entity, Relation, Sentiment, Tag
-
-__all__ = tuple(["Client", "Analysis"])
-
-
-__version__ = "0.1.0"
--- src/cro/geneea/sdk/__init__.py
+++ src/cro/geneea/sdk/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from cro.geneea.sdk._client import Client as Client
-from cro.geneea.sdk._domain import Analysis as Analysis
-from cro.geneea.sdk._domain import Entity as Entity
-from cro.geneea.sdk._domain import Relation as Relation
-from cro.geneea.sdk._domain import Sentiment as Sentiment
-from cro.geneea.sdk._domain import Tag as Tag
+
+from cro.geneea.sdk._domain import (
+    Analysis as Analysis,
+    Entity as Entity,
+    Relation as Relation,
+    Sentiment as Sentiment,
+    Tag as Tag
+)
 
 __all__ = tuple(["Client", "Analysis", "Sentiment", "Entity", "Relation", "Tag"])
 
