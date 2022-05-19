ee59f7ed8d655927d1140f129b22d407d1bcbba9
Krystof Pesek <krystof.pesek@gmail.com>
2022-05-19 14:18:08 +0200

    Batch processing bashscript added

---
+++ analyze_day_ago.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /root/irozhlas/cro-geneea-sdk/.venv/bin/activate
+source /root/irozhlas/cro-geneea-sdk/secret
+for i in `find /root/irozhlas/irozhlas-scraper/output/*.txt -newermt $(date +%Y-%m-%d -d '1 day ago') -type f -print`; do
+	cro.geneea -i $i -t analysis -f xml;
+	sleep 1;
+done
+mv /root/irozhlas/irozhlas-scraper/output/*.xml /root/irozhlas/irozhlas-scraper/output/xml
